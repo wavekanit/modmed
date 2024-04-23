a8861f11d5f44cb4e08678578c80836e5b048374
Inteoty <kittipong.tpy@gmail.com>
2024-04-23 23:56:20 +0700

    con

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
-app,get("/", (req, res) => {
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "modmed",
+    port: 8889
+});
+
+db.connect( (error) => {
+    if(error){
+        console.log(error)
+    } else {
+        console.log("MySQL Connected")
+    }
+});
+
+app.get("/", (req, res) => {
     res.send("<h1>Hello World eiei</h1>")
 });
 
-app.listen(5000, () => {
-    console.log("Server started on PORT 5000")
+app.listen(3000, () => {
+    console.log("Server started on PORT 3000")
 });
+
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '1234';
