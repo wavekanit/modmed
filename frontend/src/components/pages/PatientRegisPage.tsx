c1cf07cffc641d96e9b409b1e01c5dcb3f5dd9ab
Inteoty <kittipong.tpy@gmail.com>
2024-04-27 19:59:39 +0700

    update

--- frontend/src/components/pages/PatientRegisPage.tsx
+++ frontend/src/components/pages/PatientRegisPage.tsx
@@ -2,34 +2,47 @@ import React from "react";
 import { FormEvent, useState } from "react";
 import { useMultistepForm } from "../UseMultiForm";
 
-type Props = {}
+type Props = {};
 
 type FormData = {
-    firstName: string;
-    lastName: string;
-    idNumber: string;
-    DOB: Date;
-    sex: string;
-    address: string;
-    tell: string;
-    email: string;
-    nationality: string;
-    race: string;
-    religion: string;
-    bloodType: string;
-    eConFirstName: string;
-    eConLastName: string;
-    eConRelation: string;
-    eConTell: string;
-    eConEmail: string;
-    eConAddress: string;
-
-
-
+  firstName: string;
+  lastName: string;
+  idNumber: string;
+  DOB: Date;
+  sex: string;
+  address: string;
+  tell: string;
+  email: string;
+  nationality: string;
+  race: string;
+  religion: string;
+  bloodType: string;
+  eConFirstName: string;
+  eConLastName: string;
+  eConRelation: string;
+  eConTell: string;
+  eConEmail: string;
+  eConAddress: string;
+  allergy: {
+    type: string;
+    allergen: string;
+    status: number;
+  }[];
 };
 
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  idNumber: "",
+  DOB: new Date(),
+  sex: "",
+    address: "",
+    tell: "",
+    email:"",
+    
+
+
+
 export default function PatientRegisPage({}: Props) {
-  return (
-    <div>PatientRegisPage</div>
-  )
+  return <div>PatientRegisPage</div>;
 }
