22c4284d66b94467046f90bbfe53d93c047593fb
Inteoty <kittipong.tpy@gmail.com>
2024-04-27 19:43:03 +0700

    update

---
+++ frontend/src/components/pages/PatientRegisPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FormEvent, useState } from "react";
+import { useMultistepForm } from "../UseMultiForm";
+
+type Props = {}
+
+type FormData = {
+    firstName: string;
+    
+};
+
+export default function PatientRegisPage({}: Props) {
+  return (
+    <div>PatientRegisPage</div>
+  )
+}
--- frontend/src/components/pages/docRegisterStep/EmailRegis.tsx
+++ frontend/src/components/pages/docRegisterStep/EmailRegis.tsx
@@ -40,7 +40,7 @@ export function EmailRegis({
         <input
           type="password"
           name="password"
-          value={email}
+          value={password}
           onChange={(e) => updateFields({ password: e.target.value })}
           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="password"
@@ -54,7 +54,7 @@ export function EmailRegis({
         <input
           type="password"
           name="confirmPassword"
-          value={email}
+          value={confirmPassword}
           onChange={(e) => updateFields({ confirmPassword: e.target.value })}
           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="password"
