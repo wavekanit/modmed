3fdf18e5a03eb52956f51af6f3a44ec8444e2aa0
Minefreezey <wongsatorn.sung@hotmail.com>
2024-05-08 16:50:41 +0700

    Edited database schema according to TA suggestions

--- modmed.sql
+++ modmed.sql
@@ -5,7 +5,6 @@ CREATE DATABASE modmed
 
 USE modmed;
 
--- Test something
 
 -- Create table part        
 CREATE TABLE attendance
@@ -24,19 +23,12 @@ CREATE TABLE cure_history
   diag_result     TEXT         NOT NULL,
   methods         TEXT         NOT NULL,
   progress_status TINYINT      NOT NULL,
-  d_license       VARCHAR (10) NOT NULL,
+  d_id            INTEGER      NOT NULL,
   room_id         VARCHAR (10) NULL    ,
   date_finished   DATETIME     NULL    ,
   PRIMARY KEY (p_id, date_cure)
 );
 
-CREATE TABLE d_info
-(
-  d_id       INTEGER      NOT NULL,
-  license_id VARCHAR (10) UNIQUE  NOT NULL,
-  department VARCHAR (50) NOT NULL,
-  PRIMARY KEY (d_id)
-);
 
 CREATE TABLE edu
 (
@@ -63,24 +55,27 @@ CREATE TABLE emergency_contact
 
 CREATE TABLE employee
 (
-  id          INTEGER      NOT NULL AUTO_INCREMENT,
-  fName       VARCHAR (50) NOT NULL,
-  mName       VARCHAR (50) NULL    ,
-  lName       VARCHAR (50) NOT NULL,
-  idNumber    VARCHAR (13) NOT NULL,
-  DOB         DATE         NOT NULL,
-  sex         VARCHAR (10) NOT NULL,
-  addresses   TEXT         NOT NULL,
-  tel         VARCHAR (10) NOT NULL,
-  email       VARCHAR (50) NOT NULL,
-  pw          TEXT         NOT NULL,
-  nationality VARCHAR (50) NOT NULL,
-  race        VARCHAR (50) NOT NULL,
-  religion    VARCHAR (50) NOT NULL,
-  bloodType   VARCHAR (5)  NOT NULL,
-  e_id        INTEGER      NOT NULL,
-  relation    VARCHAR (50) NOT NULL,
-  role_name       VARCHAR (50) NOT NULL,
+  id           INTEGER      NOT NULL  AUTO_INCREMENT,
+  fName        VARCHAR (50) NOT NULL,
+  mName        VARCHAR (50) NULL    ,
+  lName        VARCHAR (50) NOT NULL,
+  idNumber     VARCHAR (13) NOT NULL,
+  DOB          DATE         NOT NULL,
+  sex          VARCHAR (10) NOT NULL,
+  addresses    TEXT         NOT NULL,
+  tel          VARCHAR (10) NOT NULL,
+  email        VARCHAR (50) NOT NULL,
+  pw           TEXT         NOT NULL,
+  nationality  VARCHAR (50) NOT NULL,
+  race         VARCHAR (50) NOT NULL,
+  religion     VARCHAR (50) NOT NULL,
+  bloodType    VARCHAR (5)  NOT NULL,
+  e_id         INTEGER      NOT NULL,
+  relation     VARCHAR (50) NOT NULL,
+  role_name        VARCHAR (50) NOT NULL,
+  d_license_id VARCHAR (10) NULL    ,
+  d_department VARCHAR (50) NULL    ,
+  quit_date    DATETIME     NULL    ,
   PRIMARY KEY (id)
 );
 
@@ -117,7 +112,7 @@ CREATE TABLE patient_allergy
 CREATE TABLE roles
 (
   role_name       VARCHAR (50) NOT NULL,
-  income_base INTEGER      NOT NULL,
+  income_base     INTEGER      NOT NULL,
   PRIMARY KEY (role_name)
 );
 
@@ -153,15 +148,10 @@ ALTER TABLE attendance
     FOREIGN KEY (id)
     REFERENCES employee (id);
 
-ALTER TABLE d_info
-  ADD CONSTRAINT FK_employee_TO_d_info
+ALTER TABLE cure_history
+  ADD CONSTRAINT FK_d_id_TO_cure_history
     FOREIGN KEY (d_id)
     REFERENCES employee (id);
-
-ALTER TABLE cure_history
-  ADD CONSTRAINT FK_d_license_TO_cure_history
-    FOREIGN KEY (d_license)
-    REFERENCES d_info (license_id);
 
 ALTER TABLE edu
   ADD CONSTRAINT FK_employee_TO_edu
@@ -204,10 +194,10 @@ INSERT INTO patient (fName,lName,idNumber,DOB,sex,addresses,tel,email,nationalit
         ('Jumpol', 'Polvichai', '3145258754234', '1980-07-04', 'Male', 'KMUTT, Bangkok', '0985745245','jumo@hotmail.com', 'Thai', 'Thai', 'Buddhist', 'O-', 2, 'Relative'),
         ('Noppakhao', 'Somsrichai', '3452122157859', '1954-04-04', 'Female','218 Pracha Uthit Rd, Bangkok', '0982385749', 'mas@hotmail.com', 'Thai', 'Thai', 'Muslim', 'B+',4,'Son');
 
-INSERT INTO employee (fName, lName, idNumber, DOB, sex, addresses, tel, email, pw, nationality, race, religion, bloodType, e_id, relation, role_name)
+INSERT INTO employee (fName, lName, idNumber, DOB, sex, addresses, tel, email, pw, nationality, race, religion, bloodType, e_id, relation, role_name, d_license_id, d_department)
     VALUES
-        ('Weera', 'Theeraphat', '3101100254685', '1940-02-09','Male', '13 Rama IX Rd., Bangkok', '0841234567', 'weera.t@modmed.com', '1234', 'Thai', 'Thai', 'Buddhist', 'A-', 1, 'Son', 'doctor'),
-        ('Phond', 'Phunchongharn', '3100111002352', '1982-09-30','Female', '126 Pracha Uthit Rd', '0562235467', 'phond.p@modmed.com', 'pp', 'Thai', 'Thai', 'Buddhist', 'A+', 4, 'Father', 'doctor'),
-        ('Acharee', 'Weerapong', '1104521345687', '1997-10-12','Female', 'Bangna, Bangkok', '0625526987', 'acharee.w@modmed.com', 'password', 'Thai', 'Thai', 'Buddhist', 'AB+', 5, 'Friend', 'finance'),
-        ('Chuchai', 'Wiwatana', '3100521245657', '1964-02-09','Male', 'Victory Monument', '0914567852', 'chuchai.w@modmed.com', '888', 'Thai', 'Thai', 'Buddhist', 'A-', 2, 'Son', 'register'),
-        ('Sanan', 'Srakaew', '3887451243216', '1960-04-23','Male', 'Srakaew', '0865986542', 'sanan.s@modmed.com', 'qwerty', 'Thai', 'Thai', 'Buddhist', 'O+', 2, 'Son', 'doctor');
+        ('Weera', 'Theeraphat', '3101100254685', '1940-02-09','Male', '13 Rama IX Rd., Bangkok', '0841234567', 'weera.t@modmed.com', '1234', 'Thai', 'Thai', 'Buddhist', 'A-', 1, 'Son', 'doctor','W74125', 'Bone'),
+        ('Phond', 'Phunchongharn', '3100111002352', '1982-09-30','Female', '126 Pracha Uthit Rd', '0562235467', 'phond.p@modmed.com', 'pp', 'Thai', 'Thai', 'Buddhist', 'A+', 4, 'Father', 'doctor', 'E41578', 'Eye'),
+        ('Acharee', 'Weerapong', '1104521345687', '1997-10-12','Female', 'Bangna, Bangkok', '0625526987', 'acharee.w@modmed.com', 'password', 'Thai', 'Thai', 'Buddhist', 'AB+', 5, 'Friend', 'finance',NULL,NULL),
+        ('Chuchai', 'Wiwatana', '3100521245657', '1964-02-09','Male', 'Victory Monument', '0914567852', 'chuchai.w@modmed.com', '888', 'Thai', 'Thai', 'Buddhist', 'A-', 2, 'Son', 'register', NULL,NULL),
+        ('Sanan', 'Srakaew', '3887451243216', '1960-04-23','Male', 'Srakaew', '0865986542', 'sanan.s@modmed.com', 'qwerty', 'Thai', 'Thai', 'Buddhist', 'O+', 2, 'Son', 'doctor', 'H19374', 'Heart');
