b121413d693b4f7b48b5e6d60db94faf73615302
Inteoty <kittipong.tpy@gmail.com>
2024-05-17 00:48:45 +0700

    add update allergy api

--- backend/Routes/patient.js
+++ backend/Routes/patient.js
@@ -19,6 +19,19 @@ db.connect( (error) => {
     }
 });
 
+router.post("/updatePatientAllergies", (req, res) => {
+    const {allergy_id, type_allergy, allergy, status_allergy} = req.body;
+    db.query("UPDATE patient_allergy SET type_allergy = ?, allergy = ?, status_allergy = ? WHERE allergy_id = ?", [type_allergy, allergy, status_allergy, allergy_id], (error, result) => {
+        if(error){
+            console.log(error);
+            res.status(500).send("Internal Server Error");
+        } else {
+            res.send("Patient Updated");
+        }
+    });
+});
+
+
 router.post("/addCure", (req, res) => {
     const {p_id, basic_symp, diag_result, methods, d_id, room_id} = req.body;
     db.query("INSERT INTO cure_history (p_id, date_cure, basic_symp, diag_result, methods, progress_status, d_id, room_id) VALUES (?,CURRENT_TIMESTAMP,?,?,?,1,?,?)", [p_id, basic_symp,diag_result, methods,d_id, room_id], (error, result) => {
@@ -69,44 +82,48 @@ router.get("/getPatient/:search", (req, res) => {
             res.send(result);
         }
     });
-    // const sqlStatement = `
-    // SELECT patient.*, 
-    //    JSON_ARRAYAGG(
-    //        JSON_OBJECT(
-    //            'type_allergy', patient_allergy.type_allergy,
-    //            'allergy', patient_allergy.allergy,
-    //            'status', patient_allergy.status_allergy
-    //        )
-    //    ) AS allergies,
-    //     JSON_OBJECT(
-    //           'fName', emergency_contact.fName,
-    //           'lName', emergency_contact.lName,
-    //           'email', emergency_contact.email,
-    //           'phone', emergency_contact.tel,
-    //           'address', emergency_contact.addresses
-    //      ) AS emergency_contact
-    // FROM patient
-    // LEFT JOIN patient_allergy ON patient.p_id = patient_allergy.p_id
-    // LEFT JOIN emergency_contact ON patient.e_id = emergency_contact.e_id
-    // GROUP BY patient.p_id;
-
-    // `;
-    // db.query(sqlStatement, (error, result) => {
-    //     if(error){
-    //         console.log(error);
-    //         res.status(500).send("Internal Server Error");
-    //     } else {
-    //         console.log(result);
-    //         const formattedResult = result.map(patient => ({
-    //             ...patient,
-    //             allergies: JSON.parse(patient.allergies)
-    //         }));
-    //         res.send(formattedResult);
-    //     }
-    // });
+    
 });
 
-
+router.get("/getPatientInfo/:id", (req, res) => {
+    const sqlStatement = `
+    SELECT patient.*, 
+       JSON_ARRAYAGG(
+           JSON_OBJECT(
+               'type_allergy', patient_allergy.type_allergy,
+               'allergy', patient_allergy.allergy,
+               'status', patient_allergy.status_allergy
+           )
+       ) AS allergies,
+       CAST(
+           JSON_OBJECT(
+               'fName', emergency_contact.fName,
+               'lName', emergency_contact.lName,
+               'email', emergency_contact.email,
+               'phone', emergency_contact.tel,
+               'address', emergency_contact.addresses
+           ) AS JSON
+       ) AS emergency_contact
+        FROM patient
+        LEFT JOIN patient_allergy ON patient.p_id = patient_allergy.p_id
+        LEFT JOIN emergency_contact ON patient.e_id = emergency_contact.e_id
+        GROUP BY patient.p_id HAVING patient.p_id = ?;
+    `;
+    db.query(sqlStatement , [req.params.id], (error, result) => {
+        if(error){
+            console.log(error);
+            res.status(500).send("Internal Server Error");
+        } else {
+            console.log(result);
+            const formattedResult = result.map(patient => ({
+                ...patient,
+                allergies: JSON.parse(patient.allergies),
+                emergency_contact: JSON.parse(patient.emergency_contact)
+            }));
+            res.send(formattedResult);
+        }
+    });
+});
 
 
 module.exports = router
