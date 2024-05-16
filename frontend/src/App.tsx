f3fec3deceb01b34d8f086ea3e94eb142c8f883a
WaveKanit <wavekanit@hotmail.com>
2024-05-17 01:27:47 +0700

    add patient search page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import PatientRegisPage from "./components/pages/patientRegisterPage/PatientRegi
 import DocInfo from "./components/pages/docInfo/DoctorInfoPage.tsx";
 import DocDetail from "./components/pages/docInfo/doctorDetailPage.tsx";
 import DocEdit from "./components/pages/docEditInfo/DocEditInfoPage";
+import PatientInfoPage from "./components/pages/patientInfo/PatientInfoPage";
 
 function App() {
   const isLoggedIn = localStorage.getItem("isLoggedIn");
@@ -30,13 +31,14 @@ function App() {
         {/* <Route path = "/crud" element={<CrudPage />} />
         <Route path = "/Add" element={<AddInfo />} />
         <Route path = "/Edit" element={<EditInfoPage />} /> */}
-        <Route path = "manage_doctor" element = {<DocInfo />} />
-        <Route path = "manage_doctor/details" element = {<DocDetail />} />
-        <Route path = "manage_doctor/details/edit" element = {<DocEdit />} />
+        <Route path = "/manage_doctor" element = {<DocInfo />} />
+        <Route path = "/manage_doctor/details" element = {<DocDetail />} />
+        <Route path = "/manage_doctor/details/edit" element = {<DocEdit />} />
         {/* <Route path = "manage_patient" element = {<CrudPage />} /> */}
         {/* <Route path = "/doctorinfo" element = {<DocInfo/>}/> */}
         {/* <Route path = "/doctorinfo/details" element = {<DocDetail/>}/> */}
         {/* <Route path = "/doctorinfo/details/edit" element = {<DocEdit/>}/> */}
+        <Route path = "/manage_patient" element = {<PatientInfoPage />} />
       </Routes>
     </>
   );
