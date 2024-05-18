0b85bc6e12bd470c2cc8da7a3a08d9fdf486bbfc
Minefreezey <wongsatorn.sung@hotmail.com>
2024-05-18 17:47:45 +0700

    updated some minor error in database

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -165,7 +165,7 @@ export default function Nav({}: Props) {
           </div>
         </nav>
       );
-    } else {
+    } else if (role === `"finance"`) {
       return <>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
